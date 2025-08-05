421aaa4546bf957c1bc5c0c53971d02e2edfe6b7
Dhruvesh Patil <160326003+Dhruvesh05@users.noreply.github.com>
2025-08-05 23:31:52 +0530

    Update page.js

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -1,10 +1,15 @@
-'use client';
-import AboutSection from '../../components/AboutSection';
+// src/app/about/page.js
+
+// import AboutSection from '../components/AboutSection';
+
+// export default function AboutPage() {
+//   return <AboutSection />;
+// }
 
 export default function AboutPage() {
   return (
-    <main>
-      <AboutSection />
-    </main>
+    <div>
+      <h1>About Page</h1>
+    </div>
   );
 }
